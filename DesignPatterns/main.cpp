e1ae53ccb041a66290c988990f820f8e7c90320f
Anton Vasilev <bysslaev@gmail.com>
2017-11-27 00:50:48 +0300

    Initial Commit

---
+++ DesignPatterns/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DesignPatterns
+//
+//  Created by admin on 27/11/2017.
+//  Copyright © 2017 Vasilev corp. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
