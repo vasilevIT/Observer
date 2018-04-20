49aa9a96879e27c6c00528f54acb93e70d1ac111
Anton Vasilev <bysslaev@gmail.com>
2018-04-20 23:54:33 +0300

    fix bugs

--- DesignPatterns/Observer.cpp
+++ DesignPatterns/Observer.cpp
@@ -8,15 +8,50 @@
 
 
 #include <iostream>
-//#include "Subject.cpp"
+#include <vector>
+using namespace std;
 
 #pragma once
-class Observer {
+class Object {
+    
+};
+
+class Observer : public Object {
 public:
     int id;
     Observer() {
         this->id = rand() % 100;
     }
     
-    virtual void update(Subject* s,double temperature, double humidity, double pressure) = 0;
+    virtual void update(Object* s,double temperature, double humidity, double pressure) = 0;
+};
+
+class Subject : public Object {
+private:
+    vector<Observer*> observers;
+    
+public:
+    void registerObserver(Observer* o){
+        this->observers.push_back(o);
+    }
+    
+    void removeObserver(Observer* o) {
+        for (int i = 0; i < this->observers.size(); i++) {
+            if (o->id == this->observers[i]->id) {
+                for (int j = i; j < this->observers.size()-1; j++) {
+                    this->observers[j] = this->observers[j + 1];
+                }
+                this->observers.pop_back();
+            }
+        }
+    }
+    
+    void notifyObservers(double temp, double humi, double press) {
+        double temperature = temp;
+        double humidity = humi;
+        double pressure = press;
+        for (int i = 0; i < this->observers.size(); i++) {
+            this->observers[i]->update(this, temperature, humidity, pressure);
+        }
+    }
 };
